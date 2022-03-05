b10809bde377004c684115bb1cd16df6ec846b44
Manuel Serret <mserret99@gmail.com>
2022-03-05 21:36:49 +0100

    fix

--- src/lib/scripts/JiraLogin.ts
+++ src/lib/scripts/JiraLogin.ts
@@ -1,5 +1,5 @@
 export function getJiraAuthorizationUrl(clientId: string): string {
-	return `https://auth.atlassian.com/authorize?audience=api.atlassian.com&client_id=${clientId}&scope=read%3Ame%20read%3Aissue-details%3Ajira&redirect_uri=${getEncodedUrlIncludingPath()}&state=YOUR_USER_BOUND_VALUE&response_type=code&prompt=consent`;
+	return `https://auth.atlassian.com/authorize?audience=api.atlassian.com&client_id=${clientId}&scope=read%3Aissue-details%3Ajira&redirect_uri=${getEncodedUrlIncludingPath()}&state=YOUR_USER_BOUND_VALUE&response_type=code&prompt=consent`;
 }
 
 export function getJiraCallbackUrl(): string {
